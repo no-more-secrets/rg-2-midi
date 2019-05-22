eb3fce147ab9b0e6d23e28924318736a42792ad6
David P. Sicilia <dpsicilia@gmail.com>
2019-05-22 18:25:31 -0400

    Use cmd line arguments to get files

--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,48 @@
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License as
-  published by the Free Software Foundation; either version 2 of the
-  License, or (at your option) any later version.  See the file
-  COPYING included with this distribution for more information.
+  published by the Free Software Foundation; either version 2 of
+  the License, or (at your option) any later version.  See the
+  file COPYING included with this distribution for more
+  information.
 */
-
-#include <iostream>
 
 #include "MidiFile.h"
 #include "RosegardenDocument.h"
 
+#include <iostream>
+
 using namespace std;
 
-int main() {
-  // Create a new blank document
-  Rosegarden::RosegardenDocument *doc =
-      new Rosegarden::RosegardenDocument(
-          true,                       // skipAutoload
-          true,                       // clearCommandHistory
-          /*m_useSequencer=*/false ); // enableSound
+#define CHECK( a, msg ) \
+  if( !( a ) ) { die( string() + msg ); }
 
-  // Read the document from the file.
-  bool readOk = doc->openDocument(
-      "/home/dsicilia/dev/revolution-now/assets/music/midi/"
-      "fife-and-drum-1.rg",
-      false, true, false );
+void die( string const& msg ) {
+  cerr << "Error: " << msg << "\n";
+  exit( 1 );
+}
 
-  if( !readOk ) {
-    cerr << "error opening document.\n";
-    return 1;
-  }
+int main( int argc, char** argv ) {
+  CHECK( argc == 3, "Usage: rg2midi in-file.rg out-file.mid" );
+
+  string rg  = argv[1];
+  string mid = argv[2];
+
+  Rosegarden::RosegardenDocument doc(
+      /*skipAutoload=*/true,
+      /*clearCommandHistory=*/true,
+      /*m_useSequencer=*/false );
+
+  bool ok;
+
+  ok = doc.openDocument( rg, /*permanent=*/false,
+                         /*squelchProgressDialog=*/true,
+                         /*enableLock=*/false );
+  CHECK( ok, "opening " + rg );
 
   Rosegarden::MidiFile midiFile;
+  ok = midiFile.convertToMidi( doc, mid );
+  CHECK( ok, "writing midi file " + mid );
 
-  if( !midiFile.convertToMidi( *doc, "out.mid" ) ) {
-    cerr << "error writing midi file.\n";
-    return 1;
-  }
   return 0;
 }
